7c7acc6537b6b4bdc6a7ae6078a967a202dffbfb
MartinSanchezHerrera <martinsanher2511@gmail.com>
2024-11-05 08:58:44 -0600

    Add files via upload

---
+++ fotoresistencia/Fotoresistencia.ino
@@ -0,0 +1,25 @@
+int ldr= A5;
+int ledPin = 13;
+
+void setup(){
+  pinMode(ledPin, OUTPUT); // Configurar LED como salida
+  pinMode(ldrPin, INPUT); // Configurar LDR como entrada
+  Serial.begin(9600); // Iniciar comunicación serie para depuración
+  
+}
+void loop(){
+  int lecturaLDR = analogRead(ldrPin); // Leer valor del LDR
+  
+  Serial.print("Valor LDR: ");
+  Serial.println(lecturaLDR); // Mostrar el valor en el monitor serie
+  
+  // Verificar si está oscuro o hay luz
+  if (lecturaLDR < umbral) {
+    digitalWrite(ledPin, HIGH); // Encender LED
+  } else {
+    digitalWrite(ledPin, LOW); // Apagar LED
+  }
+  
+  delay(500); // Espera de medio segundo antes de la siguiente lectura
+}
+
